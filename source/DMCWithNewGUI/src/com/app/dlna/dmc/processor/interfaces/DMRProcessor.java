a3d4f8e560760698400d3430eb6114859111af58
ngdacphu.khtn <ngdacphu.khtn@gmail.com>
2012-02-27 09:12:38 +0000

    Add new folder to repo

---
+++ source/DMCWithNewGUI/src/com/app/dlna/dmc/processor/interfaces/DMRProcessor.java
@@ -0,0 +1,50 @@
+package com.app.dlna.dmc.processor.interfaces;
+
+import org.teleal.cling.model.message.UpnpResponse;
+import org.teleal.cling.model.meta.Action;
+
+public interface DMRProcessor {
+	void setURI(String uri);
+
+	void setURIandPlay(String uri);
+
+	void play();
+
+	void pause();
+
+	void stop();
+
+	void seek(String position);
+
+	void setVolume(int newVolume);
+
+	int getVolume();
+
+	void addListener(DMRProcessorListner listener);
+
+	void removeListener(DMRProcessorListner listener);
+
+	void dispose();
+
+	String getName();
+
+	void setPlaylistProcessor(PlaylistProcessor playlistProcessor);
+
+	public interface DMRProcessorListner {
+		void onUpdatePosition(long current, long max);
+
+		void onPaused();
+
+		void onStoped();
+
+		void onPlaying();
+
+		void onEndTrack();
+
+		void onErrorEvent(String error);
+
+		@SuppressWarnings("rawtypes")
+		void onActionFail(Action actionCallback, UpnpResponse response, final String cause);
+	}
+
+}
