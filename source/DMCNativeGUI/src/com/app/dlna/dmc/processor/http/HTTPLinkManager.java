4322fdbdc746fbbd047146d8bc0b4c9cda768635
ngdacphu.khtn@gmail.com <ngdacphu.khtn@gmail.com@163c3a99-d2a9-5f5b-ce33-cc876903dd1f>
2012-03-19 15:01:39 +0000

    Delete projects to re-order repository

--- source/DMCNativeGUI/src/com/app/dlna/dmc/processor/http/HTTPLinkManager.java
+++
@@ -1,8 +0,0 @@
-package com.app.dlna.dmc.processor.http;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class HTTPLinkManager {
-	public static Map<String, String> LINK_MAP = new HashMap<String, String>();
-}
