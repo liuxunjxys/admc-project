a1cfa27d4f14cd636f6e8eb07e25a9328eb694f6
ngdacphu.khtn@gmail.com <ngdacphu.khtn@gmail.com@163c3a99-d2a9-5f5b-ce33-cc876903dd1f>
2012-03-19 15:52:34 +0000

    Import native project

---
+++ source/Native Application/DMCNativeGUI/src/com/app/dlna/dmc/gui/youtube/YoutubeItem.java
@@ -0,0 +1,49 @@
+package com.app.dlna.dmc.gui.youtube;
+
+public class YoutubeItem {
+	private String title;
+	private String duration;
+	private String description;
+	private String thumbnail;
+	private String url;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getThumbnail() {
+		return thumbnail;
+	}
+
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+}
