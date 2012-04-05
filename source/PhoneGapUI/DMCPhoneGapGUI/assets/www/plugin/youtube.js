7f16ad902400118d26dc07cd099eacabea227b40
ngdacphu.khtn@gmail.com <ngdacphu.khtn@gmail.com@163c3a99-d2a9-5f5b-ce33-cc876903dd1f>
2012-04-05 10:55:20 +0000

    Phonegap: youtube view: can call browse action, but not display result

--- source/PhoneGapUI/DMCPhoneGapGUI/assets/www/plugin/youtube.js
+++ source/PhoneGapUI/DMCPhoneGapGUI/assets/www/plugin/youtube.js
@@ -1,6 +1,48 @@
 var YoutubePlugin = function() {
 };
 
+YoutubePlugin.prototype.addToPlaylist = function(idx, proxy) {
+	showLoadingIcon();
+	PhoneGap.exec(null, null, 'YoutubePlugin', 'addToPlaylist', [ idx, proxy ]);
+};
+
+YoutubePlugin.prototype.query = function(queryString) {
+	showLoadingIcon();
+	PhoneGap.exec(null, null, 'YoutubePlugin', 'query', [ queryString ]);
+};
+
 PhoneGap.addConstructor(function() {
 	PhoneGap.addPlugin("YoutubePlugin", new YoutubePlugin());
 });
+
+function showYoutubeResult(e) {
+	console.log('show result');
+	console.log(e);
+	var result = eval(e);
+	for ( var i = 0; i < result.length; i++) {
+		console.log('add to list');
+		var obj = result[i];
+		yt_addItemToListView(obj);
+	}
+	myScroll_libs.scrollTo(0, 0, 0);
+	hideLoadingIcon();
+}
+
+function yt_addItemToListView(item) {
+	var html = "<li index='" + item.idx + "'";
+
+	html += "data-icon='false' ";
+
+	if (item.url != null) {
+		html += "url='" + item.url + "' ";
+	}
+	html += "onclick='onYoutubeItemClick(\'" + item.idx + "\");'>";
+	html += "<a href='#' style='padding-top: 0px;padding-bottom: 0px' data-icon='delete'><img src='"
+			+ item.thumb
+			+ "' style='height: 100%; width: height; padding-left: 4%; float: left;'/><h3>"
+			+ item.title + "</h3><p>" + item.duration + "</p></a></li>";
+	youttube_listview.append(html);
+	youttube_listview.listview('refresh');
+	youttube_listview.refresh();
+	console.log(html);
+}
--- source/PhoneGapUI/DMCPhoneGapGUI/src/com/app/dlna/dmc/phonegapui/plugin/YoutubePlugin.java
+++ source/PhoneGapUI/DMCPhoneGapGUI/src/com/app/dlna/dmc/phonegapui/plugin/YoutubePlugin.java
@@ -1,15 +1,246 @@
 package com.app.dlna.dmc.phonegapui.plugin;
 
-import org.json.JSONArray;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.ProgressDialog;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.app.dlna.dmc.phonegapui.MainActivity;
+import com.app.dlna.dmc.processor.http.HTTPServerData;
+import com.app.dlna.dmc.processor.impl.YoutubeProcessorImpl;
+import com.app.dlna.dmc.processor.interfaces.PlaylistProcessor;
+import com.app.dlna.dmc.processor.interfaces.YoutubeProcessor;
+import com.app.dlna.dmc.processor.interfaces.YoutubeProcessor.IYoutubeProcessorListener;
+import com.app.dlna.dmc.processor.playlist.PlaylistItem;
+import com.app.dlna.dmc.processor.playlist.PlaylistItem.Type;
+import com.app.dlna.dmc.processor.youtube.YoutubeItem;
 import com.phonegap.api.Plugin;
 import com.phonegap.api.PluginResult;
+import com.phonegap.api.PluginResult.Status;
 
 public class YoutubePlugin extends Plugin {
+	private static final String TAG = YoutubePlugin.class.getName();
+	private static final String ACTION_QUERY = "query";
+	private static final String ACTION_ADD_TO_PLAYLIST = "addToPlaylist";
+	private List<YoutubeItem> m_resultList;
+	private static ProgressDialog PROGRESS_DIALOG = null;
 
 	@Override
 	public PluginResult execute(String action, JSONArray data, String callbackId) {
+
+		if (ACTION_ADD_TO_PLAYLIST.equals(action)) {
+			// Add to playlist
+			Log.i(TAG, "add to playlist");
+			try {
+				int idx = data.getInt(0);
+				boolean proxy = data.getBoolean(1);
+				if (m_resultList != null && idx < m_resultList.size() - 1) {
+					YoutubeItem item = m_resultList.get(idx);
+					String title = item.getTitle();
+					String link = item.getUrl();
+					if (proxy) {
+						executeProxyMode(title, link);
+					} else {
+						executeNoProxy(title, link);
+					}
+				}
+
+			} catch (JSONException e) {
+				e.printStackTrace();
+				return new PluginResult(Status.JSON_EXCEPTION);
+			}
+
+		} else if (ACTION_QUERY.equals(action)) {
+			// query
+			Log.i(TAG, "query");
+			try {
+				String query = data.getString(0);
+
+				new YoutubeProcessorImpl().executeQuery(query, new IYoutubeProcessorListener() {
+
+					@Override
+					public void onStartPorcess() {
+						showProgress();
+					}
+
+					@Override
+					public void onSearchComplete(List<YoutubeItem> result) {
+						dismissProgress();
+						JSONArray array = new JSONArray();
+						m_resultList = result;
+						for (int i = 0; i < result.size(); ++i) {
+							array.put(createJsonFromYoutubeItem(result.get(i), i));
+						}
+						sendJavascript("showYoutubeResult('" + array.toString() + "');");
+						Log.i(TAG, "resutl = " + array.toString());
+					}
+
+					@Override
+					public void onFail(Exception ex) {
+						dismissProgress();
+					}
+
+					@Override
+					public void onGetDirectLinkComplete(String result) {
+						dismissProgress();
+					}
+				});
+			} catch (JSONException e) {
+				e.printStackTrace();
+				return new PluginResult(Status.JSON_EXCEPTION);
+			}
+		}
+
 		return null;
 	}
 
+	private JSONObject createJsonFromYoutubeItem(YoutubeItem item, int idx) {
+		JSONObject result = new JSONObject();
+		try {
+			result.put("idx", String.valueOf(idx));
+			result.put("title", item.getTitle());
+			result.put("thumb", item.getThumbnail());
+			result.put("duration", item.getDuration());
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	private void insertPlaylistItem(final String title, final String link, final String directlink) {
+		PlaylistProcessor playlistProcessor = MainActivity.UPNP_PROCESSOR.getPlaylistProcessor();
+		if (playlistProcessor != null) {
+			PlaylistItem item = new PlaylistItem();
+			item.setTitle(title);
+			item.setUrl(directlink);
+			item.setType(Type.VIDEO);
+			if (playlistProcessor.addItem(item))
+				Toast.makeText(MainActivity.INSTANCE, "Add item \"" + item.getTitle() + "\" to playlist sucess",
+						Toast.LENGTH_SHORT).show();
+			else {
+				if (playlistProcessor.isFull()) {
+					Toast.makeText(MainActivity.INSTANCE, "Current playlist is full", Toast.LENGTH_SHORT).show();
+				} else {
+					Toast.makeText(MainActivity.INSTANCE, "Item already exits in current Playlist", Toast.LENGTH_SHORT)
+							.show();
+				}
+			}
+		}
+
+	}
+
+	private void executeProxyMode(final String title, final String link) {
+		try {
+			YoutubeProcessor m_youtubeProcessor = new YoutubeProcessorImpl();
+			m_youtubeProcessor.registURL(link, new IYoutubeProcessorListener() {
+
+				@Override
+				public void onStartPorcess() {
+					showProgress();
+				}
+
+				@Override
+				public void onFail(Exception ex) {
+					dismissProgress();
+					ex.printStackTrace();
+				}
+
+				@Override
+				public void onSearchComplete(List<YoutubeItem> result) {
+					dismissProgress();
+				}
+
+				@Override
+				public void onGetDirectLinkComplete(String result) {
+					dismissProgress();
+					String generatedURL;
+					try {
+						generatedURL = new URI("http", HTTPServerData.HOST + ":" + HTTPServerData.PORT, result, null,
+								null).toString();
+						Log.i(TAG, "Generated URL = " + generatedURL);
+						insertPlaylistItem(title, link, generatedURL);
+					} catch (URISyntaxException e) {
+						e.printStackTrace();
+					}
+
+				}
+			});
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private void executeNoProxy(final String title, final String link) {
+		try {
+			YoutubeProcessor m_youtubeProcessor = new YoutubeProcessorImpl();
+			m_youtubeProcessor.getDirectLink(link, new IYoutubeProcessorListener() {
+
+				@Override
+				public void onStartPorcess() {
+					showProgress();
+				}
+
+				@Override
+				public void onGetDirectLinkComplete(String result) {
+					dismissProgress();
+					insertPlaylistItem(title, link, result);
+				}
+
+				@Override
+				public void onFail(Exception ex) {
+					dismissProgress();
+				}
+
+				@Override
+				public void onSearchComplete(List<YoutubeItem> result) {
+					dismissProgress();
+
+				}
+
+			});
+		} catch (Exception ex) {
+			Toast.makeText(MainActivity.INSTANCE, ex.getMessage(), Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	public static void showProgress() {
+		// Log.i(TAG, "show progress");
+		// if (PROGRESS_DIALOG == null) {
+		// PROGRESS_DIALOG = new ProgressDialog(MainActivity.INSTANCE);
+		// PROGRESS_DIALOG.setMessage("Contact to Youtube server");
+		// PROGRESS_DIALOG.setTitle("Loading...");
+		// PROGRESS_DIALOG.setCancelable(true);
+		// }
+		// MainActivity.INSTANCE.runOnUiThread(new Runnable() {
+		//
+		// @Override
+		// public void run() {
+		// PROGRESS_DIALOG.show();
+		// Log.i(TAG, "showing...");
+		// }
+		// });
+
+	}
+
+	public static void dismissProgress() {
+		// MainActivity.INSTANCE.runOnUiThread(new Runnable() {
+		//
+		// @Override
+		// public void run() {
+		// Log.i(TAG, "dissmis progress");
+		// if (PROGRESS_DIALOG != null) {
+		// PROGRESS_DIALOG.dismiss();
+		// }
+		// }
+		// });
+
+	}
 }
