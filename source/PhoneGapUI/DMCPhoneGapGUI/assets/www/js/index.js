90a3a29346a103e2d3cc8251e89bf346e1daa29e
DeoSieuMau <DeoSieuMau@163c3a99-d2a9-5f5b-ce33-cc876903dd1f>
2012-04-06 07:03:03 +0000

    Resolved problems with animation buttons Add some new resources Note: view at the end of index.js file for some button-state information

--- source/PhoneGapUI/DMCPhoneGapGUI/assets/www/js/index.js
+++ source/PhoneGapUI/DMCPhoneGapGUI/assets/www/js/index.js
@@ -10,15 +10,17 @@ var dmr_listview; // list dmr
 var library_listview;
 var youttube_listview;
 
-var time_to_swap_image = 300;
+var time_to_swap_image = 200;
 
 $(document).ready(function() {
 	iScrollConfig();
 	myInitPage();
 	myInitDevicesSide();
+	myInitYoutubeSide();
 	myInitPlaylistsSide();
 });
 
+//=============================GLOBAL MEDTHODS==========================================
 function iScrollConfig() {
 	setTimeout(function() {
 		myScroll_devi_dmS = new iScroll('div_wrraper_devi_dmS', {
@@ -48,15 +50,34 @@ function changeImagePath(sender, imgPath) {
 	$(sender).attr('src', imgPath);
 }
 
-function changeImagePath(sender, imgPath, timeOut) {
+function changeImagePathWithTimeOut (sender, imgPath, timeOut){
 	setTimeout(function() {
 		$(sender).attr('src', imgPath);
 	}, timeOut);
 }
-/* Index side */
+
+function changeStateImage (sender){
+	var currentPath = $(sender).attr('data-current-path');
+	var stateAImgPath = $(sender).attr('data-state-a');
+	var stateBImgPath = $(sender).attr('data-state-b');
+	if (currentPath == stateAImgPath){
+		$(sender).attr('data-current-path', stateBImgPath);
+	}else{
+		$(sender).attr('data-current-path', stateAImgPath);
+	}
+	changeImagePathWithTimeOut(sender, $(sender).attr('data-current-path'), time_to_swap_image);
+	var currentState = $(sender).attr('data-my-state');
+	if (currentState == 'true'){
+		currentState = 'false';
+	}else{
+		currentState = 'true';
+	}
+}
+//=========================================PAGE SIDE=================================
 function myInitPage() {
 	$('div.div_subcontent').hide(); // all hide
 	$('div#content_devices').show(); // first tab appear
+	$('div#div_navbar_1 a').addClass('ui-btn-active');
 	$('div[data-role="navbar"] a').live('click', function() {
 		$('div.div_subcontent').hide();
 		var datahref_info = $(this).attr('data-href');
@@ -88,7 +109,6 @@ function myInitPage() {
 		}
 		$(this).addClass('ui-btn-active');
 	});
-	$('div#div_navbar_1 a').addClass('ui-btn-active');
 	// init variable
 	// view devices:
 	dms_listview = $('#div_wrraper_devi_dmS ul:first');
@@ -98,6 +118,31 @@ function myInitPage() {
 
 	icon_loading = $('#icon_loading');
 	icon_loading.hide();
+	
+	//Mapping process for all image on page
+	$('.img_normal').live('vmousedown', function() {
+		changeImagePath($(this), $(this).attr('data-highlight-image'));
+	});
+	
+	$('.img_normal').live('vmouseup', function() {
+		changeImagePathWithTimeOut($(this), $(this).attr('data-normal-image'), time_to_swap_image);
+	});
+	
+	$('.img_double_state').live('vmousedown', function(){
+		var currentPath = $(this).attr('data-current-path');
+		var stateAImgPath = $(this).attr('data-state-a');
+		var highlightImagePath = "";
+		if (currentPath == stateAImgPath){
+			highlightImagePath = $(this).attr('data-highlight-a');
+		}else{
+			highlightImagePath = $(this).attr('data-highlight-b');
+		}
+		changeImagePath($(this), highlightImagePath);
+	});
+	
+	$('.img_double_state').live('vmouseup', function() {
+		changeImagePathWithTimeOut($(this), $(this).attr('data-current-path'), time_to_swap_image);
+	});
 }
 
 /* User interface */
@@ -109,7 +154,7 @@ function hideLoadingIcon() {
 	icon_loading.hide();
 }
 
-/* Devices side */
+//================================================DEVICES SIDE===========================
 function myInitDevicesSide() {
 	$('div#content_devices div.div_devi_subcontent').hide();
 	$('div#div_devi_dmS').show();
@@ -139,7 +184,7 @@ function myInitDevicesSide() {
 				}
 			});
 }
-/* Library side */
+//==========================================LIBRARY SIDE=================================
 function onClick_previousResult() {
 	console.log('view previous result');
 	var btn_prev = $('#btn_prevPage');
@@ -189,7 +234,18 @@ function disablePrevPageButton() {
 	$('#btn_prevPage').attr("enable", "false");
 }
 
-/* Youtube side */
+//=============================================YOUTUBE SIDE==============================
+function myInitYoutubeSide (){
+	$('#div_you_toolbar_right img').bind('tap', function (){
+		onClick_search_you ($(this));
+	});
+	
+	$('#div_you_proxy img').bind('tap', function (){
+		onClick_activateProxy_you ($(this));
+	});
+	
+}
+
 // Button search event
 function onClick_search_you(sender) {
 	console.log('Search');
@@ -199,59 +255,132 @@ function onClick_search_you(sender) {
 // Button use proxy event
 function onClick_activateProxy_you(sender) {
 	var state = $(sender).attr('data-my-state');
-	if (state == "activated") {
-		// Code deactivate here
-		console.log('deactivated process');
-
-		$(sender).attr('data-my-state', 'deactivated');
-	} else {
-		// Code activate here
-		console.log('activated process');
-
-		$(sender).attr('data-my-state', 'activated');
+	if (state == "true"){ //activated
+		changeStateImage(sender);
+	}else{
+		changeStateImage(sender);
 	}
 }
 
-/* Playlists side */
-function myInitPlaylistsSide() {
-	$('#div_field_seekbar input').bind('change', function(event, ui) {
-		console.log('onChange');
-		console.log(event);
-		console.log(ui);
+//==============================================PLAYLISTS SIDE==============================
+
+function myInitPlaylistsSide (){
+	$('#img_media_control_previous').bind('tap', function(){
+		onClick_previous_play ($(this));
 	});
+	
+	$('#img_media_control_stop').bind('tap', function(){
+		onClick_stop_play ($(this));
+	});
+	
+	$('#img_media_control_next').bind('tap', function(){
+		onClick_next_play ($(this));
+	});
+	
+	$('#img_media_control_play').bind('tap', function(){
+		onClick_play_play($(this));
+	});
+	
+	$('#img_media_control_volume').bind('tap', function(){
+		onClick_volume_play($(this));
+	});
+	
+	$('#div_field_seekbar input').bind('change', function(){
+		onChange_durationBar($(this));
+	})
+	
+	$('#div_play_volume_left input').bind('change', function(){
+		onChange_volumeBar($(this));
+	})
+}
+//Parameter note: sender is seekbar (slider) that you want to set it values
+//This method for init-time (if needed - in myInitPlaylistsSide method)
+//By default (in html file): 
+//	duration-seekbar: currentValue = 0, maxValue = 300
+//	volume-seekbar: currentValue = 50, maxValue = 50
+function setValueForSeekBar (sender, currentValue, maxValue){
+	$(sender).attr('max', maxValue);
+	$(sender).attr('value', currentValue);
 }
 
-/* Playlists side */
-function onClick_play_play(sender) {
+//SEEK BAR EVENT-----------------------------------
+//Seek bar - duration of content
+function onChange_durationBar (sender){
+	var currentValue = $(sender).attr('value');
+	var maxValue = $(sender).attr('max');
+	
+	console.log('max: ' + currentValue);
+	console.log('current: ' + maxValue);
+}
+
+//Seek bar - volume
+function onChange_volumeBar (sender){
+	var currentValue = $(sender).attr('value');
+	var maxValue = $(sender).attr('max');
+	
+	console.log('max: ' + currentValue);
+	console.log('current: ' + maxValue);
+}
+
+
+//MEDIA CONTROLER EVENT-----------------------------
+//OnClick event of PLAY button
+function onClick_play_play (sender){
 	var state = $(sender).attr('data-my-state');
-	if (state == "play") {
-		changeImagePath(sender, 'img/media_pause_icon.png', time_to_swap_image);
-		$(sender).attr('data-my-state', 'pause');
-
+	if (state == "true"){
+		//convert to pausing state
+		console.log('convert to PAUSING state');
+		changeStateImage(sender);//call when successful
 	} else {
-		changeImagePath(sender, 'img/media_play_icon.png', time_to_swap_image);
-		$(sender).attr('data-my-state', 'play');
-
+		//convert to playing state
+		console.log('convert to PLAYING state');
+		changeStateImage(sender);//call when successful
 	}
 }
 
-function onMouseDown_play_play(sender) {
+//OnClick event of NEXT button
+function onClick_next_play (sender){
+	console.log('CLick event on NEXT button');
+}
+
+//OnClick event of PREVIOUS button
+function onClick_previous_play (sender){
+	console.log('CLick event on PREVIOUS button');
+}
+
+//OnClick event of STOP button
+function onClick_stop_play (sender){
+	console.log('CLick event on STOP button');
+}
+
+//OnClick event of VOLUME button{
+function onClick_volume_play (sender){
 	var state = $(sender).attr('data-my-state');
-	if (state == "play") {
-		changeImagePath(sender, 'img/media_play_icon_hl.png');
-	} else {
-		changeImagePath(sender, 'img/media_pause_icon_hl.png');
+	if (state == "true"){
+		//convert to mute state
+		console.log('convert to MUTE state');
+		changeStateImage(sender);//call when successful
+	}else{
+		//convert to activate state
+		console.log('convert to ACTIVATED state');
+		changeStateImage(sender);//call when successful
 	}
 }
 
-function onClick_next_play(sender) {
-	changeImagePath(sender, 'img/media_next_icon.png', time_to_swap_image);
-}
-
-function onClick_previous_play(sender) {
-	changeImagePath(sender, 'img/media_previous_icon.png', time_to_swap_image);
-}
-
-function onClick_stop_play(sender) {
-	changeImagePath(sender, 'img/media_stop_icon.png', time_to_swap_image);
-}
+/*
+NOTICE: About state of double-state button (be defined in data-my-state attribute)
+On Youtube-view:
+	+ Proxy button:
+		- true: proxy-mode is ACTIVATED
+		- false: proxy-mode is DEACTIVATED
+		* default: false
+On Playlists-view:
+	+ Play/Pause button:
+		- true: on PLAYING state
+		- false: on PLAUSING state
+		* default: false
+	+ Volume button:
+		- true: on ACTIVATED mode
+		- false: on MUTE mode
+		* default: true
+*/
