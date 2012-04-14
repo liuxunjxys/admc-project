0644f1d443f29fdd288e59d8b42a5256265ca103
ngdacphu.khtn <ngdacphu.khtn@gmail.com>
2012-04-14 01:03:32 +0000

    Import version 2

---
+++ source/Native Application/src/com/app/dlna/dmc/processor/download/DownloadURLThread.java
@@ -0,0 +1,20 @@
+package com.app.dlna.dmc.processor.download;
+
+import java.io.File;
+
+import android.app.NotificationManager;
+import android.content.Context;
+
+public class DownloadURLThread extends DownloadThread {
+	public DownloadURLThread(String name, String url, File parrent, DownloadListener listener, int downloadID, Context context) {
+		m_name = name;
+		m_url = url;
+		m_maxsize = -1;
+		m_parent = parrent;
+		m_listener = listener;
+		m_downloadID = downloadID;
+		m_context = context;
+		m_notificationManager = (NotificationManager) m_context.getSystemService(Context.NOTIFICATION_SERVICE);
+	}
+
+}
