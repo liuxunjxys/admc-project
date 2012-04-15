aa063b8ed507bb0a7f6532f18f27e7c18a869366
ngdacphu.khtn@gmail.com <ngdacphu.khtn@gmail.com@163c3a99-d2a9-5f5b-ce33-cc876903dd1f>
2012-04-15 18:48:51 +0000

    Import version 3

---
+++ source/Native Application/DMCNativeGUI_V3.0/src/com/app/dlna/dmc/gui/customview/localnetwork/LocalNetworkArrayAdapter.java
@@ -0,0 +1,160 @@
+package com.app.dlna.dmc.gui.customview.localnetwork;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.teleal.cling.model.meta.Device;
+import org.teleal.cling.model.meta.Icon;
+import org.teleal.cling.model.meta.RemoteDevice;
+import org.teleal.cling.support.model.DIDLObject;
+import org.teleal.cling.support.model.container.Container;
+import org.teleal.cling.support.model.item.ImageItem;
+import org.teleal.cling.support.model.item.MusicTrack;
+import org.teleal.cling.support.model.item.VideoItem;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import app.dlna.controller.R;
+
+public class LocalNetworkArrayAdapter extends ArrayAdapter<ListviewItem> {
+	private LayoutInflater m_inflater = null;
+	private Map<String, Bitmap> m_cacheDMSIcon;
+
+	public LocalNetworkArrayAdapter(Context context, int textViewResourceId) {
+		super(context, textViewResourceId);
+		m_inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		m_cacheDMSIcon = new HashMap<String, Bitmap>();
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if (convertView == null) {
+			convertView = m_inflater.inflate(R.layout.lvitem_localnetwork, null, false);
+		}
+		if (convertView.getTag() == null) {
+			setViewHolder(convertView);
+		}
+		final ListviewItem object = getItem(position);
+
+		final ViewHolder holder = (ViewHolder) convertView.getTag();
+		if (object.getData() instanceof Device)
+			setDeviceItem((Device) object.getData(), holder);
+		else
+			setDIDLObject((DIDLObject) object.getData(), holder);
+
+		return convertView;
+	}
+
+	@SuppressWarnings("rawtypes")
+	private void setDeviceItem(final Device device, final ViewHolder holder) {
+		holder.name.setText(device.getDetails().getFriendlyName());
+		final String udn = device.getIdentity().getUdn().getIdentifierString();
+
+		if (device instanceof RemoteDevice) {
+			holder.desc.setText(((RemoteDevice) device).getIdentity().getDescriptorURL().getAuthority());
+		} else {
+			holder.desc.setText("Local device");
+		}
+
+		if (m_cacheDMSIcon.containsKey(udn)) {
+			holder.icon.setImageBitmap(m_cacheDMSIcon.get(udn));
+		} else {
+			if (device instanceof RemoteDevice) {
+				try {
+					final Icon[] icons = device.getIcons();
+					if (icons != null && icons[0] != null && icons[0].getUri() != null) {
+
+						new Thread(new Runnable() {
+
+							@Override
+							public void run() {
+								try {
+									final RemoteDevice remoteDevice = (RemoteDevice) device;
+
+									String urlString = remoteDevice.getIdentity().getDescriptorURL().getProtocol() + "://"
+											+ remoteDevice.getIdentity().getDescriptorURL().getAuthority()
+											+ icons[0].getUri().toString();
+									URL url = new URL(urlString);
+									final Bitmap icon = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+									m_cacheDMSIcon.put(udn, icon);
+									// m_mainActivity.runOnUiThread(new
+									// Runnable() {
+									//
+									// @Override
+									// public void run() {
+									// DeviceArrayAdapter.this.notifyDataSetChanged();
+									// }
+									// });
+
+								} catch (Exception ex) {
+									// m_mainActivity.runOnUiThread(new
+									// Runnable() {
+									//
+									// @Override
+									// public void run() {
+									// holder.icon.setImageResource(R.drawable.icon_dms);
+									// }
+									// });
+									holder.icon.setImageBitmap(null);
+									ex.printStackTrace();
+								}
+							}
+						}).start();
+
+					}
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			} else {
+				holder.icon.setImageResource(R.drawable.ic_launcher);
+			}
+		}
+
+	}
+
+	private void setDIDLObject(DIDLObject object, ViewHolder holder) {
+		holder.name.setText(object.getTitle());
+		if (object.getId().equals("-1")) {
+			holder.icon.setVisibility(View.GONE);
+			return;
+		}
+		holder.icon.setVisibility(View.VISIBLE);
+		if (object instanceof Container) {
+			holder.icon.setImageResource(R.drawable.ic_didlobject_container);
+		} else {
+			if (object instanceof MusicTrack) {
+				holder.icon.setImageResource(R.drawable.ic_didlobject_audio);
+			} else if (object instanceof VideoItem) {
+				holder.icon.setImageResource(R.drawable.ic_didlobject_video);
+			} else if (object instanceof ImageItem) {
+				holder.icon.setImageResource(R.drawable.ic_didlobject_image);
+			} else {
+				holder.icon.setImageResource(R.drawable.ic_didlobject_unknow);
+			}
+		}
+	}
+
+	public void setViewHolder(View view) {
+		ViewHolder viewHolder = new ViewHolder();
+		viewHolder.name = (TextView) view.findViewById(R.id.name);
+		viewHolder.desc = (TextView) view.findViewById(R.id.desc);
+		viewHolder.icon = (ImageView) view.findViewById(R.id.icon);
+		view.setTag(viewHolder);
+	}
+
+	private class ViewHolder {
+		TextView desc;
+		TextView name;
+		ImageView icon;
+	}
+
+}
---
+++ source/Native Application/DMCNativeGUI_V3.0/src/com/app/dlna/dmc/processor/interfaces/DMSProcessor.java
@@ -0,0 +1,33 @@
+package com.app.dlna.dmc.processor.interfaces;
+
+import java.util.List;
+import java.util.Map;
+
+import org.teleal.cling.support.model.DIDLObject;
+
+public interface DMSProcessor {
+	void browse(String objectID, int pageIndex, DMSProcessorListner listener);
+
+	void nextPage(DMSProcessorListner listener);
+
+	void previousPage(DMSProcessorListner listener);
+
+	void back(DMSProcessorListner listener);
+
+	void dispose();
+
+	public interface DMSProcessorListner {
+		void onBrowseComplete(String objectID, boolean haveNext, boolean havePrev,
+				Map<String, List<? extends DIDLObject>> result);
+
+		void onBrowseFail(String message);
+	}
+
+	DIDLObject getDIDLObject(String objectID);
+
+	void addAllToPlaylist(PlaylistProcessor playlistProcessor);
+
+	void removeAllFromPlaylist(PlaylistProcessor playlistProcessor);
+
+	List<DIDLObject> getAllObjects();
+}
