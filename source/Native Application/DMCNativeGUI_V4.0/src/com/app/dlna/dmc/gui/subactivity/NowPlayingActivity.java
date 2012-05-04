632d7d4d2e874e64f49a944052e4be3f3324c72a
ngdacphu.khtn@gmail.com <ngdacphu.khtn@gmail.com@163c3a99-d2a9-5f5b-ce33-cc876903dd1f>
2012-05-04 19:41:34 +0000

    fixed local player feature here

--- source/Native Application/DMCNativeGUI_V4.0/src/com/app/dlna/dmc/gui/subactivity/NowPlayingActivity.java
+++ source/Native Application/DMCNativeGUI_V4.0/src/com/app/dlna/dmc/gui/subactivity/NowPlayingActivity.java
@@ -6,19 +6,17 @@ import java.net.MalformedURLException;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import app.dlna.controller.v4.R;
 
 import com.app.dlna.dmc.gui.MainActivity;
-import com.app.dlna.dmc.gui.customview.nowplaying.MultiContentSurfaceView;
-import com.app.dlna.dmc.gui.customview.nowplaying.MultiContentSurfaceView.ImageOverlayData;
 import com.app.dlna.dmc.gui.customview.nowplaying.RendererControlView;
 import com.app.dlna.dmc.gui.customview.nowplaying.TopToolbarView;
 import com.app.dlna.dmc.processor.async.AsyncTaskWithProgressDialog;
@@ -40,8 +38,9 @@ public class NowPlayingActivity extends Activity implements Callback {
 	// private Animation m_animFlipInPrevious;
 	// private Animation m_animFlipOutPrevious;
 	// private boolean m_waiting;
-	private MultiContentSurfaceView m_surface;
+	private SurfaceView m_surface;
 	private SurfaceHolder m_holder;
+	private ImageView m_image;
 	private TextView m_title;
 
 	@Override
@@ -51,25 +50,15 @@ public class NowPlayingActivity extends Activity implements Callback {
 		m_rendererControl = (RendererControlView) findViewById(R.id.rendererControlView);
 		m_topToolbar = (TopToolbarView) findViewById(R.id.topToolbar);
 
-		// m_viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
 		m_progressDialog = new ProgressDialog(NowPlayingActivity.this);
 		m_progressDialog.setTitle("Loading image");
 		m_progressDialog.setCancelable(true);
 
-		// m_animFlipInNext = AnimationUtils.loadAnimation(this,
-		// R.anim.flipinnext);
-		// m_animFlipOutNext = AnimationUtils.loadAnimation(this,
-		// R.anim.flipoutnext);
-		// m_animFlipInPrevious = AnimationUtils.loadAnimation(this,
-		// R.anim.flipinprevious);
-		// m_animFlipOutPrevious = AnimationUtils.loadAnimation(this,
-		// R.anim.flipoutprevious);
-
-		ActivitySwipeDetector activitySwipeDetector = new ActivitySwipeDetector(this);
-		// m_viewFlipper.setOnTouchListener(activitySwipeDetector);
-
-		m_surface = (MultiContentSurfaceView) findViewById(R.id.surface);
-		m_surface.setOnTouchListener(activitySwipeDetector);
+		SwipeDetector swipeDetector = new SwipeDetector(this);
+		m_image = (ImageView) findViewById(R.id.image);
+		m_image.setOnTouchListener(swipeDetector);
+		m_surface = (SurfaceView) findViewById(R.id.surface);
+		m_surface.setOnTouchListener(swipeDetector);
 		m_holder = m_surface.getHolder();
 		m_holder.addCallback(this);
 		m_holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
@@ -84,13 +73,6 @@ public class NowPlayingActivity extends Activity implements Callback {
 
 				@Override
 				public void run() {
-					// if (m_waiting)
-					// return;
-					// m_viewFlipper.setInAnimation(m_animFlipInPrevious);
-					// m_viewFlipper.setOutAnimation(m_animFlipOutPrevious);
-					// m_viewFlipper.getInAnimation().setAnimationListener(m_animationListner);
-					// m_viewFlipper.showPrevious();
-					// updateCurrentPlaylistItem();
 					updateItemInfo();
 				}
 			});
@@ -102,13 +84,6 @@ public class NowPlayingActivity extends Activity implements Callback {
 
 				@Override
 				public void run() {
-					// if (m_waiting)
-					// return;
-					// m_viewFlipper.setInAnimation(m_animFlipInNext);
-					// m_viewFlipper.setOutAnimation(m_animFlipOutNext);
-					// m_viewFlipper.getInAnimation().setAnimationListener(m_animationListner);
-					// m_viewFlipper.showNext();
-					// updateCurrentPlaylistItem();
 					updateItemInfo();
 				}
 			});
@@ -149,40 +124,30 @@ public class NowPlayingActivity extends Activity implements Callback {
 		if (item == null)
 			return;
 		m_title.setText(item.getTitle());
-		// ImageView iv = new ImageView(NowPlayingActivity.this);
 		switch (item.getType()) {
 		case AUDIO: {
-			// iv.setImageDrawable(getResources().getDrawable(R.drawable.ic_didlobject_audio_large));
-			// m_surface.setVisibility(View.GONE);
-			// m_viewFlipper.setVisibility(View.VISIBLE);
-			// m_viewFlipper.addView(iv);
-			m_surface.setImageOverlay(new ImageOverlayData(BitmapFactory.decodeResource(getResources(),
-					R.drawable.ic_didlobject_audio_large), ""));
+			m_image.setVisibility(View.VISIBLE);
+			m_image.setImageDrawable(getResources().getDrawable(R.drawable.ic_didlobject_audio_large));
+			m_surface.setVisibility(View.GONE);
 			break;
 		}
 		case VIDEO: {
 			if (MainActivity.UPNP_PROCESSOR.getDMRProcessor() instanceof LocalDMRProcessorImpl) {
-				// m_viewFlipper.setVisibility(View.GONE);
-				// m_surface.setVisibility(View.VISIBLE);
-				m_surface.setImageOverlay(new ImageOverlayData(null, ""));
-
+				m_surface.setVisibility(View.VISIBLE);
+				m_image.setVisibility(View.GONE);
 			} else {
-				// m_surface.setVisibility(View.GONE);
-				// m_viewFlipper.setVisibility(View.VISIBLE);
-				// m_viewFlipper.addView(iv);
-				// iv.setImageDrawable(getResources().getDrawable(R.drawable.ic_didlobject_video_large));
-				m_surface.setImageOverlay(new ImageOverlayData(BitmapFactory.decodeResource(getResources(),
-						R.drawable.ic_didlobject_video_large), ""));
+				m_image.setVisibility(View.VISIBLE);
+				m_image.setImageDrawable(getResources().getDrawable(R.drawable.ic_didlobject_video_large));
+				m_surface.setVisibility(View.GONE);
 			}
 			break;
 		}
 		case IMAGE: {
-			// m_viewFlipper.setVisibility(View.VISIBLE);
-			// iv.setImageDrawable(null);
 			new AsyncTaskWithProgressDialog<String, Void, Bitmap>("Loading image") {
 
 				protected void onPreExecute() {
-					// m_waiting = true;
+					m_image.setVisibility(View.VISIBLE);
+					m_surface.setVisibility(View.GONE);
 				};
 
 				@Override
@@ -204,27 +169,10 @@ public class NowPlayingActivity extends Activity implements Callback {
 
 				@Override
 				protected void onPostExecute(Bitmap result) {
-					// super.onPostExecute(result);
-					// m_waiting = false;
-					// ImageView iv = new ImageView(NowPlayingActivity.this);
 					if (result == null) {
-						// iv.setImageDrawable(getResources().getDrawable(R.drawable.ic_didlobject_image_large));
-						// m_viewFlipper.addView(iv);
-						// ImageOverlayData data = new ImageOverlayData();
-						// data.bm = result;
-						// data.url = "";
-						m_surface.setImageOverlay(new ImageOverlayData(BitmapFactory.decodeResource(getResources(),
-								R.drawable.ic_didlobject_image_large), ""));
-						updateSurfaceImage();
+						m_image.setImageDrawable(getResources().getDrawable(R.drawable.ic_didlobject_image_large));
 					} else {
-						Log.i(TAG, "w = " + result.getWidth() + "; h = " + result.getHeight());
-						// iv.setImageBitmap(result);
-						// m_viewFlipper.addView(iv);
-						// ImageOverlayData data = new ImageOverlayData();
-						// data.bm = result;
-						// data.url = "";
-						m_surface.setImageOverlay(new ImageOverlayData(result, ""));
-						updateSurfaceImage();
+						m_image.setImageBitmap(result);
 					}
 				}
 			}.execute(new String[] { item.getUrl(), "512", "512" });
@@ -239,6 +187,7 @@ public class NowPlayingActivity extends Activity implements Callback {
 			dmrProcessor.setURIandPlay(item);
 		}
 		m_topToolbar.setCurrentSpinnerSelected(item);
+		updateSurfaceView();
 	}
 
 	// private void updateCurrentPlaylistItem() {
@@ -281,23 +230,6 @@ public class NowPlayingActivity extends Activity implements Callback {
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 		updateSurfaceView();
-		updateSurfaceImage();
-	}
-
-	private void updateSurfaceImage() {
-		Canvas canvas = null;
-		try {
-			canvas = m_holder.lockCanvas(null);
-			synchronized (m_holder) {
-				m_surface.onDraw(canvas);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (canvas != null) {
-				m_holder.unlockCanvasAndPost(canvas);
-			}
-		}
 	}
 
 	@Override
@@ -312,14 +244,18 @@ public class NowPlayingActivity extends Activity implements Callback {
 	}
 
 	private void updateSurfaceView() {
-		if (MainActivity.UPNP_PROCESSOR.getDMRProcessor() instanceof LocalDMRProcessorImpl) {
-			LocalDMRProcessorImpl localDMR = (LocalDMRProcessorImpl) MainActivity.UPNP_PROCESSOR.getDMRProcessor();
-			if (m_surface.getVisibility() == View.VISIBLE) {
-				localDMR.getPlayer().setDisplay(m_holder);
-			} else {
-				localDMR.getPlayer().setDisplay(null);
-			}
+		try {
+			if (MainActivity.UPNP_PROCESSOR.getDMRProcessor() instanceof LocalDMRProcessorImpl) {
+				LocalDMRProcessorImpl localDMR = (LocalDMRProcessorImpl) MainActivity.UPNP_PROCESSOR.getDMRProcessor();
+				if (m_surface.getVisibility() == View.VISIBLE) {
+					localDMR.getPlayer().setDisplay(m_holder);
+				} else {
+					localDMR.getPlayer().setDisplay(null);
+				}
 
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
 	}
 
