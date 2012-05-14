ec78406bed847da03712b11bd4e386ada32ec2ff
ngdacphu.khtn@gmail.com <ngdacphu.khtn@gmail.com@163c3a99-d2a9-5f5b-ce33-cc876903dd1f>
2012-05-14 17:50:03 +0000

    import v3

---
+++ source/PhoneGapUI/DMCPhoneGapGUI_Version3/assets/www/js/tabControl/tabscontrol.js
@@ -0,0 +1,32 @@
+var currentTab_TabsControl;
+
+function initTabs (){
+	currentTab_TabsControl = "library";
+	
+	init_LibraryTab();
+	init_NowplayingTab();
+	init_SettingField();
+	
+	$('#div_navbar_1').bind('tap', function (){
+		switchingLibrarytab ();
+	});
+	$('#div_navbar_2').bind('tap', function (){
+		switchingNowplayingTab ();
+	});
+}
+
+function switchingLibrarytab (){
+	if (currentTab_TabsControl != "library"){
+		hide_NowplayingTab();
+		show_LibraryTab();
+		currentTab_TabsControl = "library";
+	}
+}
+
+function switchingNowplayingTab (){
+	if (currentTab_TabsControl != "nowplaying"){
+		hide_LibraryTab();
+		show_NowplayingTab();
+		currentTab_TabsControl = "nowplaying";
+	}
+}
