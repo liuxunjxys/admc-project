be6eb5ae6bb97fc4a9e2c92441bea9033e1f56c7
ngdacphu.khtn@gmail.com <ngdacphu.khtn@gmail.com@163c3a99-d2a9-5f5b-ce33-cc876903dd1f>
2012-06-04 17:40:08 +0000

    Native: re-enable download feature. Remove custom MimeTypeMap

--- source/Native Application/DMCNativeGUI_V4.0/src/com/app/dlna/dmc/processor/download/DownloadItemThread.java
+++ source/Native Application/DMCNativeGUI_V4.0/src/com/app/dlna/dmc/processor/download/DownloadItemThread.java
@@ -9,13 +9,17 @@ import android.content.Context;
 
 public class DownloadItemThread extends DownloadThread {
 	public DownloadItemThread(DIDLObject item, File parrent, DownloadListener listener, int downloadID, Context context) {
-		m_name = item.getTitle();
-		m_url = item.getResources().get(0).getValue();
-		m_maxsize = item.getResources().get(0).getSize();
-		m_parent = parrent;
-		m_listener = listener;
-		m_downloadID = downloadID;
-		m_context = context;
-		m_notificationManager = (NotificationManager) m_context.getSystemService(Context.NOTIFICATION_SERVICE);
+		super(context);
+		try {
+			m_name = item.getTitle();
+			m_url = item.getResources().get(0).getValue();
+			m_maxsize = item.getResources().get(0).getSize();
+			m_parent = parrent;
+			m_listener = listener;
+			m_downloadID = downloadID;
+			m_context = context;
+		} catch (Exception ex) {
+			m_name = null;
+		}
 	}
 }
